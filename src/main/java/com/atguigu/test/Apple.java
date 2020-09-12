2c362e8b8b2c1b557d8bbcbc0b0f2098e890cdf9
lqs649646508 <lqs649646508@aliyun.com>
2020-09-12 12:01:05 +0800

    git stu

--- src/main/java/com/atguigu/test/Apple.java
+++ src/main/java/com/atguigu/test/Apple.java
@@ -3,6 +3,7 @@ package com.atguigu.test;
 public class Apple {
 
 	public static void main(String[] args) {
+		System.out.println("hhhh");
 		System.out.println("git hello project");
 		System.out.println("git hello project 111");
 	}
